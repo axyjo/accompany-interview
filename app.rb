81bf433bac5cc4a919ebc4ce8314d3344bd49900
Akshay Joshi <me@akshayjoshi.com>
2016-05-23 23:52:37 -0400

    Add stub for sources and events.

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 require 'sinatra/base'
+require 'sinatra/json'
 require 'tilt/erb'
 
 class AccompanyInterview < Sinatra::Application
   get '/' do
     erb :index
+  end
+
+  get '/sources' do
+    return json []
+  end
+
+  get '/events/:provider/:id' do
+    return json []
   end
 end
